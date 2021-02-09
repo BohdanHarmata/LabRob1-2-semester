7366e35c6122c6923ccd42379f336ff3d025178c
BohdanHarmata <70683703+BohdanHarmata@users.noreply.github.com>
2021-02-09 12:16:55 +0200

    Add files via upload

---
+++ LabRob3/LabRob3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LabRob3
+{
+    class Program
+    {
+        delegate int One (int x);
+        delegate int Two(int y);
+        delegate int Three(int z);
+        delegate int Avarage (ref int []arr);
+
+        static void Main(string[] args)
+        {
+            One one = delegate (int x)
+            {
+                return x;
+            };
+            Two two = delegate (int y)
+            {
+                return y;
+            };
+            Three three = delegate (int z)
+            {
+                return z;
+            };
+            Random r = new Random();
+            var x = one(r.Next());
+            var y = two(r.Next());
+            var z = three(r.Next());
+
+
+            int[] array = new int[] { x, y, z };
+            for (int i = 0; i < array.Length; i++) 
+            {
+
+                Console.WriteLine(" Число в массиві " + array[i]);
+            }
+ 
+            Avarage arr = delegate (ref int[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    int avg = (array[i] + array[i+1] + array[i+2]) / array.Length;
+                    return avg;
+                }
+                return 0;
+            };
+            var avarage = arr(ref array);
+            Console.WriteLine("Середнє арифметичне " + avarage);
+
+        }
+    }
+}
---
+++ Labrob1/Labrob1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Labrob1
+{
+    class Program
+    {
+
+        delegate int Avarage(int a, int b, int c);
+        static void Main(string[] args)
+        {
+            Avarage avg = delegate (int a, int b, int c)
+            {
+                return (a + b + c) / 3;
+            };
+
+            var avarage = avg(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Середнье арифметичне дорівнює " + avarage);
+        }
+    }
+}
